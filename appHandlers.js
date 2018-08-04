d2549f48b26bc7518e3ae66cefccd9bef84dd3cc
jacobhjustice <jacobhj1997@gmail.com>
2018-08-04 12:02:49 -0500

    Added group display and login to group

--- appHandlers.js
+++ appHandlers.js
@@ -1,16 +1,33 @@
-function login(){
+function login() {
     GMConsole.token = document.getElementById("groupmeLogin").value;
     GMConsole.groups = [];
    _login();
 }
 
+function selectGroup() {
+    print("SELECT")
+}
+
 function _login() {
     function cb(data) {
-        var arr = JSON.parse(data);
+        var arr = JSON.parse(data).response;
         for(var i = 0; i < arr.length; i++) {
             GMConsole.groups.push(arr[i]);
         }
+        document.getElementById("content").innerHTML = "";
+        _displayGroups();
     };
 
     httpRequest('https://api.groupme.com/v3/groups?token=' + GMConsole.token, false, cb)
 };
+
+function _displayGroups(){
+    document.getElementById("instruction").innerHTML = "Select which group you would like to operate within:";
+    var groups = GMConsole.groups;
+    var html = "";
+    for(var i = 0; i < groups.length; i++) {
+        html += "<label class = 'groupLabel'><input type = 'radio' class = 'group' name = 'group' value = '" + i + "'/>" + groups[i].name + "</label>";
+    }
+    html += '<div class = "btn" id = "selectGroup" onclick = "selectGroup()">Select</div>';
+    document.getElementById("content").innerHTML = html;
+}
